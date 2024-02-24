f983d4945973346fb745cff48a660c05c070ae37
thetheOrange <13807527538@163.com>
2024-02-24 12:38:33 +0800

    清除调试信息

--- test/demo.py
+++ test/demo.py
@@ -74,9 +74,9 @@ def generate_video():
 
 # 程序入口
 def main():
-    cookie_str = "nostalgia_conf=-1; _uuid=A6CD253D-10958-AC510-C662-A62BBF9752C181733infoc; buvid_fp=e539f0e67670e66a942f189f404e5f10; buvid3=5FA73BC1-383F-E18A-B539-3D015E64BE4284112infoc; buvid4=E973217A-57A6-EF60-9238-7FB72CA817C884170-023060617-L5Q1kH26y1aAQnTJXzhtuA%3D%3D; b_nut=1702607453; CURRENT_FNVAL=4048; rpdid=|(kmJYl)m|YY0J'uY)Y|RkY)m; i-wanna-go-back=-1; b_ut=5; FEED_LIVE_VERSION=V8; header_theme_version=CLOSE; home_feed_column=5; browser_resolution=1536-703; bp_video_offset_1780794180=899064035198107649; CURRENT_QUALITY=80; fingerprint=e539f0e67670e66a942f189f404e5f10; buvid_fp_plain=undefined; hit-new-style-dyn=1; hit-dyn-v2=1; LIVE_BUVID=AUTO2316861462458256; PVID=1; CURRENT_BLACKGAP=0; is-2022-channel=1; enable_web_push=DISABLE; SESSDATA=c5608d91%2C1723618810%2C71f6b%2A22CjCMOrXaiFO45bZdVrb9g7gLfF6OxsybEm13jJVTVH_29CsQ4s9sRdNBhWzRfUzajdoSVi03MFVNbGlWRjVqYlgxNTFQdENiZGdHOFRlMndPWEFfWjQtdllBR3RsUllnOFc1YngzbXdra20yenBIWVNzZFJRMWZZeU82R0RIR3poQXpFdEF0Y1N3IIEC; bili_jct=9f82a70d8335600f3fbae7fe05cbaf45; DedeUserID=1780794180; DedeUserID__ckMd5=6adafff5be6d2849; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDgzMjYwMTcsImlhdCI6MTcwODA2Njc1NywicGx0IjotMX0.x6iVJLxXskyhK8-4-CqUjZ5WU6zEUxtvKgwrmRLEmxk; bili_ticket_expires=1708325957; b_lsid=8423DB3A_18DB7623DBB; sid=4yqn4426"
+    cookie_str = ""
     cookies = {temp[:temp.find("=")]: temp[temp.find("=") + 1:] for temp in cookie_str.split("; ")}
-    base_url = "https://www.bilibili.com/video/BV1E4411G7m5/?spm_id_from=333.337.search-card.all.click&vd_source=1975c316afe56823772171b01ff0e77f"
+    base_url = ""
     cid, bv = get_cid(base_url)
     video_url, audio_url = get_url(base_url, bv, cid, cookies)
     download(base_url, video_url, audio_url)
